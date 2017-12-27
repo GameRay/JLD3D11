3f823e1398a1b5d9939976d7ed1ef9c86dc59e58
GameRay <359877454@qq.com>
2017-12-27 20:11:06 +0800

    添加GameTime的部分代码

---
+++ Lighting/GameTimer.cpp
@@ -0,0 +1,85 @@
+#include <Windows.h>//查询计时器函数包含在此
+#include "GameTimer.h"
+
+
+GameTimer::GameTimer()
+	:mSecondsPerCount(0.0),mDeltaTime(-1.0),mBaseTime(0),
+	mPausedTime(0),mPrevTime(0),mCurrTime(0),mStopped(false)
+{
+	__int64 countPerSec;
+	QueryPerformanceFrequency((LARGE_INTEGER*)&countPerSec);
+	mSecondsPerCount=1.0/(double)countPerSec;
+}
+
+float GameTimer::TotalTime() const
+{
+	//当前处于暂停状态//
+	//                     |<--paused time-->|
+	// ----*---------------*-----------------*------------*------------*------> time
+	//  mBaseTime       mStopTime        startTime     mStopTime    mCurrTime
+	//  启动时的时刻 开始停止的时间		 暂停切换到开始的时刻
+	if (mStopped)
+	{
+
+		return(float)(mStopTime-mPausedTime-mBaseTime)*mSecondsPerCount;
+
+	}
+	//  (mCurrTime - mPausedTime) - mBaseTime 
+	//
+	//                     |<--paused time-->|
+	// ----*---------------*-----------------*------------*------> time
+	//  mBaseTime       mStopTime        startTime     mCurrTime
+	//非暂停状态
+	else
+	{
+
+		return(float)(mCurrTime-mPausedTime-mBaseTime)*mSecondsPerCount;
+
+	}
+
+}
+
+float GameTimer::DeltaTime() const
+{
+	return(float)mDeltaTime;
+}
+
+float GameTimer::Reset()
+{
+
+	__int64 currTime;
+	QueryPerformanceCounter((LARGE_INTEGER*)&currTime);
+
+	mBaseTime=currTime;
+	mPrevTime=currTime;
+	mStopTime=0;
+	mStopped=false;
+}
+
+void GameTimer::Start()
+{
+	__int64 startTime;
+	QueryPerformanceCounter((LARGE_INTEGER*)&startTime);
+
+	if(mStopped)
+	{
+		mPausedTime+=(startTime-mStopTime);
+		mPrevTime=startTime;
+		mStopTime=0;
+		mStopped=false;
+
+	}
+
+}
+
+void GameTimer::Stop()
+{
+	if (!mStopped)
+	{
+		__int64 currTime;
+		QueryPerformanceCounter((LARGE_INTEGER*)&currTime);
+		mStopTime=currTime;
+		mStopped=true;
+	}
+		
+}
